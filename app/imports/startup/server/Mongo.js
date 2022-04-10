78a10503b608e5f290977fb7dc9d594bbd715604
timothyro <timr808@gmail.com>
2022-04-09 15:27:07 -1000

    created default data and schemas for beaches, hikes, spots, and views

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Beaches } from '../../api/location/Beach';
+import { Hikes } from '../../api/location/Hike';
+import { Spots } from '../../api/location/Spot';
+import { Views } from '../../api/location/View';
 
 /* eslint-disable no-console */
 
@@ -9,10 +13,58 @@ function addData(data) {
   Stuffs.collection.insert(data);
 }
 
+function addBeach(data) {
+  console.log(`  Adding: ${data.name}`);
+  Beaches.collection.insert(data);
+}
+
+function addHike(data) {
+  console.log(`  Adding: ${data.name}`);
+  Hikes.collection.insert(data);
+}
+
+function addSpot(data) {
+  console.log(`  Adding: ${data.name}`);
+  Spots.collection.insert(data);
+}
+
+function addView(data) {
+  console.log(`  Adding: ${data.name}`);
+  Views.collection.insert(data);
+}
+
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+if (Beaches.collection.find().count() === 0) {
+  if (Meteor.settings.defaultBeaches) {
+    console.log('Creating Beach data.');
+    Meteor.settings.defaultBeaches.map(data => addBeach(data));
+  }
+}
+
+if (Hikes.collection.find().count() === 0) {
+  if (Meteor.settings.defaultHikes) {
+    console.log('Creating Hike data.');
+    Meteor.settings.defaultHikes.map(data => addHike(data));
+  }
+}
+
+if (Spots.collection.find().count() === 0) {
+  if (Meteor.settings.defaultSpots) {
+    console.log('Creating Spot data.');
+    Meteor.settings.defaultSpots.map(data => addSpot(data));
+  }
+}
+
+if (Views.collection.find().count() === 0) {
+  if (Meteor.settings.defaultViews) {
+    console.log('Creating View data.');
+    Meteor.settings.defaultViews.map(data => addView(data));
   }
 }
