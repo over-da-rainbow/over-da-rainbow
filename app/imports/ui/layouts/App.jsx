1d19c8ec80acaa14b2fde9acabacd1e25975c320
Kevin Nahinu <54668347+kaleinah@users.noreply.github.com>
2022-04-14 09:28:41 -1000

    WIP-2

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -9,12 +9,16 @@ import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
 import ListStuff from '../pages/ListStuff';
 import ListStuffAdmin from '../pages/ListStuffAdmin';
+import ListBeaches from "../pages/ListBeaches";
+import ListHikes from "../pages/ListHikes";
+import ListViews from "../pages/ListViews";
 import AddStuff from '../pages/AddStuff';
 import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
+
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -31,7 +35,6 @@ class App extends React.Component {
             <ProtectedRoute path="/beach" component={ListBeaches}/>
               <ProtectedRoute path="/hike" component={ListHikes}/>
               <ProtectedRoute path="/view" component={ListViews}/>
-
             <ProtectedRoute path="/add" component={AddStuff}/>
             <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
             <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
