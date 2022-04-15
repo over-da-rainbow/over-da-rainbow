e5951ef4ef420dd07e5f4c1e6a8374791e17cb4e
Rubegen <rjacobo@hawaii.edu>
2022-04-14 14:17:58 -1000

    fixed up aesthetics of landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,32 +5,29 @@ import { Container, Grid, Header, Image } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-
-      <div>
-        <div className='scenic-background-image'>
-          <Grid id='landing-page' centered columns={3} divided='vertically' style={{ paddingTop: 100 }}>
-            <Grid.Column>
-              <Image style={{ width: 500 }}centered circular src='../images/overdarainbow-logo.png'/>
-            </Grid.Column>
-          </Grid>
-        </div>
+      <div className='scenic-background-image'>
+        <Grid id='landing-page' centered columns={3} divided='vertically' style={{ paddingTop: 100 }}>
+          <Grid.Column>
+            <Image style={{ width: 500 }}centered circular src='../images/overdarainbow-logo.png'/>
+          </Grid.Column>
+        </Grid>
         <Container centered>
-          <Grid divided={'vertically'} centered columns={2}>
-            <Grid.Row>
+          <Grid style={{ padding: 70 }} divided={'vertically'} centered columns={2}>
+            <Grid.Row style={{ paddingTop: 70 }}>
               <Grid.Column>
-                <Header as='h1'>Discover</Header>
+                <Header className={'big-letters'} inverted as='h1'>Discover</Header>
               </Grid.Column>
               <Grid.Column>
-                <Header as='h2'>Adventure to new places with friends and classmates</Header>
+                <Header style={{ fontFamily: 'Academy Engraved LET', textAlign: 'center' }} inverted as='h1'>Adventure to new places with friends and classmates</Header>
               </Grid.Column>
             </Grid.Row>
 
-            <Grid.Row>
+            <Grid.Row style={{ paddingTop: 70 }}>
               <Grid.Column>
-                <Header as='h2'>Share your review and input with friends</Header>
+                <Header style={{ fontFamily: 'Academy Engraved LET', textAlign: 'center' }} inverted as='h1'>Share your experience and learn about others' by leaving a review</Header>
               </Grid.Column>
               <Grid.Column>
-                <Header as='h1'>Review</Header>
+                <Header className={'big-letters'} inverted as='h1'>Review</Header>
               </Grid.Column>
             </Grid.Row>
           </Grid>
