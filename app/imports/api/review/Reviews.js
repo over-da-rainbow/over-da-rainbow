9258ee40c25aed306d0bb32a1241c6d18ea92f46
Cole House <30875198+Cole-House@users.noreply.github.com>
2022-04-21 10:48:41 -1000

    Review Schema

---
+++ app/imports/api/review/Reviews.js
@@ -0,0 +1,34 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The StuffsCollection. It encapsulates state and variable values for stuff.
+ */
+class ReviewsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ReviewsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      title: String,
+      rating: Number,
+      review: String,
+      owner: String,
+      createdAt: Date,
+      locationId: String,
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+  }
+}
+
+/**
+ * The singleton instance of the StuffsCollection.
+ * @type {StuffsCollection}
+ */
+export const Reviews = new ReviewsCollection();
