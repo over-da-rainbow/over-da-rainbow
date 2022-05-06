c083b04ed221306e8e5521678a9361aedd30cace
timothyro <timr808@gmail.com>
2022-05-06 09:23:04 -1000

    link to locations and new layout

--- app/imports/ui/components/PlannedEvent.jsx
+++ app/imports/ui/components/PlannedEvent.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
-import { Feed } from 'semantic-ui-react';
+import { Feed, Loader } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { withRouter, NavLink } from 'react-router-dom';
+import { Beaches } from '../../api/location/Beach';
+import { Hikes } from '../../api/location/Hike';
+import { Spots } from '../../api/location/Spot';
+import { Views } from '../../api/location/View';
+import { Volunteer } from '../../api/location/Volunteer';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class PlannedEvent extends React.Component {
   render() {
+    let doc = null;
+    if (Beaches.collection.findOne({ name: this.props.plannedEvent.location }) != null) {
+      doc = Beaches.collection.findOne({ name: this.props.plannedEvent.location });
+    } else if (Hikes.collection.findOne({ name: this.props.plannedEvent.location }) != null) {
+      doc = Hikes.collection.findOne({ name: this.props.plannedEvent.location });
+    } else if (Spots.collection.findOne({ name: this.props.plannedEvent.location }) != null) {
+      doc = Spots.collection.findOne({ name: this.props.plannedEvent.location });
+    } else if (Views.collection.findOne({ name: this.props.plannedEvent.location }) != null) {
+      doc = Views.collection.findOne({ name: this.props.plannedEvent.location });
+    } else if (Volunteer.collection.findOne({ name: this.props.plannedEvent.location }) != null) {
+      doc = Volunteer.collection.findOne({ name: this.props.plannedEvent.location });
+    }
     return (
       <Feed.Event>
         <Feed.Content>
-          <Feed.User as='a'>{this.props.plannedEvent.name}&apos;s Event</Feed.User>
-          <Feed.Date>{this.props.plannedEvent.datetime}</Feed.Date>
+          <Feed.Summary>
+            <Feed.User as='a'>{this.props.plannedEvent.name}</Feed.User>&apos;s Event
+            <Feed.Date>{this.props.plannedEvent.datetime}</Feed.Date>
+          </Feed.Summary>
           <Feed.Meta>
-            <div>Location: {this.props.plannedEvent.location}</div>
             <div>{this.props.plannedEvent.title}</div>
+            <NavLink to={`/location/${doc._id}`}>Location: {this.props.plannedEvent.location}</NavLink>
           </Feed.Meta>
         </Feed.Content>
       </Feed.Event>
--- app/imports/ui/pages/Events.jsx
+++ app/imports/ui/pages/Events.jsx
@@ -6,6 +6,11 @@ import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Events } from '../../api/message/Event';
 import PlannedEvent from '../components/PlannedEvent';
+import { Beaches } from '../../api/location/Beach';
+import { Hikes } from '../../api/location/Hike';
+import { Spots } from '../../api/location/Spot';
+import { Views } from '../../api/location/View';
+import { Volunteer } from '../../api/location/Volunteer';
 
 /** Renders a table containing all of the View documents. Use <CardComponent> to render each row. */
 class Event extends React.Component {
@@ -20,7 +25,7 @@ class Event extends React.Component {
     return (
       <Container id='event-page'>
         <Header as="h2" textAlign="center">Event Board</Header>
-        <Feed>
+        <Feed size="large">
           {this.props.events.map((plannedEvent, index) => <PlannedEvent key={index} plannedEvent={plannedEvent}/>)}
         </Feed>
         <Button as={NavLink} to={'/addevent'}>Plan an Event</Button>
@@ -38,9 +43,14 @@ Event.propTypes = {
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Events.userPublicationName);
+  const eventsub = Meteor.subscribe(Events.userPublicationName);
+  const beachsub = Meteor.subscribe(Beaches.userPublicationName);
+  const hikesub = Meteor.subscribe(Hikes.userPublicationName);
+  const spotsub = Meteor.subscribe(Spots.userPublicationName);
+  const viewsub = Meteor.subscribe(Views.userPublicationName);
+  const volunteersub = Meteor.subscribe(Volunteer.userPublicationName);
   // Determine if the subscription is ready
-  const ready = subscription.ready();
+  const ready = eventsub.ready() && beachsub.ready() && hikesub.ready() && spotsub.ready() && viewsub.ready() && volunteersub.ready();
   // Get the Stuff documents
   const events = Events.collection.find({}).fetch();
   return {
